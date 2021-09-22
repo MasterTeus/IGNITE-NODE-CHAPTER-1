2ddb95b483c4e108c6221f0c8f68a0400d65ce95
Mateus Cazuza <39800209+MasterTeus@users.noreply.github.com>
2021-09-22 16:35:59 -0300

    feat: Deve ser possível buscar o extrato bancário do cliente

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ const { v4: uuid } = require("uuid");
 
 const app = express();
 app.use(express.json());
+
 const users = [];
+
+function verifyIfExistsAccount(req, res, next) {
+  const { cpf } = req.headers;
+
+  const userExists = users.find((user) => user.cpf === cpf);
+
+  if (!userExists) {
+    return res.status(401).json({ error: "User not exists" });
+  }
+
+  req.userExists = userExists;
+
+  return next();
+}
 
 /**
  * cpf: string,
@@ -12,6 +27,7 @@ const users = [];
  * statment: []
  */
 
+//TODO: Deve ser possível criar uma conta
 app.post("/account", (req, res) => {
   const { name, cpf } = req.body;
 
@@ -31,6 +47,13 @@ app.post("/account", (req, res) => {
   });
 
   return res.status(201).json(users);
+});
+
+//TODO: Deve ser possível buscar o extrato bancário do clie nte
+app.get("/statment", verifyIfExistsAccount, (req, res) => {
+  const { userExists } = req;
+
+  return res.status(201).json({ statment: userExists.statment });
 });
 
 app.listen(3333);
