b65703aa615171ea0d85a2881722a675042f149f
Mateus Cazuza <39800209+MasterTeus@users.noreply.github.com>
2021-09-22 16:07:03 -0300

    feat: add user

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 const express = require("express");
+const { v4: uuid } = require("uuid");
 
 const app = express();
+app.use(express.json());
+const users = [];
+
+/**
+ * cpf: string,
+ * name: string,
+ * id: uuid,
+ * statment: []
+ */
+
+app.post("/account", (req, res) => {
+  const { name, cpf } = req.body;
+
+  const userAlreadyExists = users.some((user) => user.cpf === cpf);
+
+  if (userAlreadyExists) {
+    return res.status(400).json({
+      error: "User already exists"
+    });
+  }
+
+  users.push({
+    cpf,
+    name,
+    id: uuid(),
+    statment: []
+  });
+
+  return res.status(201).json(users);
+});
 
 app.listen(3333);
